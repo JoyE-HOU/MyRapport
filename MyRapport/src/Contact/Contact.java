12bc989960d62d3d8f3b243988696bdae617aac8
“Joy” <joyevans@me.com>
2023-07-04 11:34:57 -0500

    Created Contact class

---
+++ MyRapport/src/Contact/Contact.java
@@ -0,0 +1,29 @@
+package Contact;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public void setLastNAme(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
